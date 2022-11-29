b12ad1ce180522d74fe449e17ecfb754d61040b3
Ricardogn224 <abrahamricardogn224@gmail.com>
2022-11-29 16:57:22 +0100

    Q8

--- Q6.sql
+++ Q6.sql
@@ -8,7 +8,7 @@ DECLARE
     customerId int;
 BEGIN
 
-  IF EXISTS (SELECT albumId=album.id  FROM album,album_stock  WHERE album.id = album_stock.id AND album.name = ALBUM_NAME  AND album_stock.stock > 0
+  IF EXISTS (SELECT albumId=album.id  FROM album,album_stock  WHERE album.id = album_stock.id AND album.name = ALBUM_NAME  AND album_stock.stock > 0 
   AND (SELECT customerId = id FROM customer WHERE email = CUSTOMER_EMAIL ))
 
   THEN
